9736d66aacb3a37973a2b6a17760c06d0f42ff7a
Dian Mukti Wibowo <onyetcorp@gmail.com>
2025-12-30 23:20:22 +0700

    Inisiasi Proyek Kiblat

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -8,7 +8,7 @@ import GoogleMaps
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
-    GMSServices.provideAPIKey("AIzaSyDHY1o10ybRp_9IptE66I63LLtj7pgRPFQ")
+    GMSServices.provideAPIKey("{google_api_key}")
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
